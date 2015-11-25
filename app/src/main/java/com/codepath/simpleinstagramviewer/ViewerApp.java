3138d01bb8efa785b39c80e0822fc255fd21cebf
Venu Venkatraman <vvenkatraman@vvenkatraman-macbookpro.local>
2015-11-25 12:36:56 -0800

    More updates and refactoring

--- app/src/main/java/com/codepath/simpleinstagramviewer/ViewerApp.java
+++
@@ -1,9 +0,0 @@
-package com.codepath.simpleinstagramviewer;
-
-import android.app.Application;
-
-/**
- * Created by vvenkatraman on 11/17/15.
- */
-public class ViewerApp extends Application {
-}
--- app/src/main/java/com/codepath/simpleinstagramviewer/activity/PopularPictureViewerActivity.java
+++ app/src/main/java/com/codepath/simpleinstagramviewer/activity/PopularPictureViewerActivity.java
@@ -1,9 +1,13 @@
 package com.codepath.simpleinstagramviewer.activity;
 
 import android.content.Context;
+import android.content.res.TypedArray;
+import android.graphics.Canvas;
+import android.graphics.drawable.Drawable;
 import android.os.Build;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
+import android.support.v4.content.ContextCompat;
 import android.support.v4.widget.SwipeRefreshLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
@@ -14,7 +18,6 @@ import android.transition.TransitionInflater;
 import android.view.View;
 
 import com.codepath.simpleinstagramviewer.R;
-import com.codepath.simpleinstagramviewer.adapter.DividerItemDecoration;
 import com.codepath.simpleinstagramviewer.data.DataLoader;
 
 import java.lang.ref.WeakReference;
@@ -79,5 +82,47 @@ public class PopularPictureViewerActivity extends AppCompatActivity implements
 
     public RecyclerView getRecyclerView() {
         return recyclerView;
+    }
+
+    public class DividerItemDecoration extends RecyclerView.ItemDecoration {
+
+        private final int[] ATTRS = new int[]{android.R.attr.listDivider};
+
+        private Drawable mDivider;
+
+        /**
+         * Default divider will be used
+         */
+        public DividerItemDecoration(Context context) {
+            final TypedArray styledAttributes = context.obtainStyledAttributes(ATTRS);
+            mDivider = styledAttributes.getDrawable(0);
+            styledAttributes.recycle();
+        }
+
+        /**
+         * Custom divider will be used
+         */
+        public DividerItemDecoration(Context context, int resId) {
+            mDivider = ContextCompat.getDrawable(context, resId);
+        }
+
+        @Override
+        public void onDraw(Canvas c, RecyclerView parent, RecyclerView.State state) {
+            int left = parent.getPaddingLeft();
+            int right = parent.getWidth() - parent.getPaddingRight();
+
+            int childCount = parent.getChildCount();
+            for (int i = 0; i < childCount; i++) {
+                View child = parent.getChildAt(i);
+
+                RecyclerView.LayoutParams params = (RecyclerView.LayoutParams) child.getLayoutParams();
+
+                int top = child.getBottom() + params.bottomMargin;
+                int bottom = top + mDivider.getIntrinsicHeight();
+
+                mDivider.setBounds(left, top, right, bottom);
+                mDivider.draw(c);
+            }
+        }
     }
 }
